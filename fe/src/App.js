dffde43cb13adf68ffbbae48d7ee75a092adcd1e
Phạm Trần Khôi <82496203+khoipham160766@users.noreply.github.com>
2022-09-23 01:17:31 +0700

    setup

--- fe/src/App.js
+++ fe/src/App.js
@@ -2,10 +2,11 @@ import { Fragment } from 'react';
 import LayoutUser from './layout/user';
 import './App.css';
 
+
 function App() {
   return (
     <Fragment>
-      abc
+        <LayoutUser/>
     </Fragment>
   );
 }
