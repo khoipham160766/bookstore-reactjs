92072039cb3bfbf6ebda9ffa78471a5b1a4a5b06
Phạm Trần Khôi <82496203+khoipham160766@users.noreply.github.com>
2022-09-23 00:35:12 +0700

    setup project

---
+++ fe/src/components/user/header/index.jsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+import "./style.css"
+
+const Header = () => {
+    return(
+        <Fragment>
+            
+        </Fragment>
+    )
+}
+
+export default Header;
