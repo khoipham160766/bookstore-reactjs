d53b984e9338c8cbe5d9975b8139de519d63a71e
Phạm Trần Khôi <82496203+khoipham160766@users.noreply.github.com>
2022-09-23 00:38:13 +0700

    setup

---
+++ fe/src/layout/user/index.jsx
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+import Header from "../../components/user/header";
+import "./style.css";
+
+const LayoutUser = () => {
+    return(
+        <Fragment>
+
+        </Fragment>
+    )
+}
+
+export default LayoutUser;
