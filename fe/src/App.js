b5780df104ecd35f3a86e04dd138d8398732b0c4
Phạm Trần Khôi <82496203+khoipham160766@users.noreply.github.com>
2022-09-28 00:52:07 +0700

    update

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,12 +1,29 @@
 import { Fragment } from 'react';
 import LayoutUser from './layout/user';
+import HomePage from './pages/user/homePage';
+import ContactPage from './pages/user/contactPage';
+import ProductsPage from './pages/user/productsPage';
+import LoginPage from './pages/user/loginPage';
+import RegisterPage from './pages/user/registerPage';
+import ForgotPasswordPage from './pages/user/forgotPasswordPage';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 
 
 function App() {
   return (
     <Fragment>
-        <LayoutUser/>
+      <Routes>
+        <Route path="/" element={<LayoutUser/>}>
+            <Route path="contact" element={<ContactPage/>} />
+            <Route path="product" element={<ProductsPage/>} />
+            <Route path="home" element={<HomePage/>} />
+            <Route index element={<HomePage/>} />
+        </Route>
+        <Route path='/login' element={<LoginPage/>}/>
+        <Route path='/register' element={<RegisterPage/>}/>
+        <Route path='/forgotpassword' element={<ForgotPasswordPage/>}/>
+      </Routes>
     </Fragment>
   );
 }
