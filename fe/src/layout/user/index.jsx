c7cfab7dfad735a2e4feeeb152510d1f2281876c
Phạm Trần Khôi <82496203+khoipham160766@users.noreply.github.com>
2022-10-05 00:34:37 +0700

    master layout user

--- fe/src/layout/user/index.jsx
+++ fe/src/layout/user/index.jsx
@@ -1,11 +1,21 @@
 import React, { Fragment } from "react";
-import Header from "../../components/user/header";
+// import Header from "../../components/user/header";
+import Navigation from "../../components/user/navbar";
+import Slider from "../../components/user/carousel";
+import Footer from "../../components/user/footer";
+import { Outlet } from "react-router-dom";
 import "./style.css";
 
 const LayoutUser = () => {
     return(
         <Fragment>
-
+            {/* <Header /> */}
+            <Navigation />
+            <Slider />
+            <div className="main-content">
+                <Outlet />
+            </div>
+            <Footer />
         </Fragment>
     )
 }
