7399f0e5031a31fc38150269eaf6f6701ca7c67e
Phạm Trần Khôi <82496203+khoipham160766@users.noreply.github.com>
2022-10-08 19:53:16 +0700

    complete pages/homePage

---
+++ fe/src/pages/user/homePage/index.jsx
@@ -0,0 +1,52 @@
+import React, { Fragment } from "react";
+import "./style.css";
+import SpecialRowProduct from "../../../components/user/product/specialRowProduct";
+import NormalRowProduct from "../../../components/user/product/normalRowProduct";
+
+const HomePage = () => {
+    const config_screen_swiper = {
+        0: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        480: {
+            slidesPerView: 2,
+            spaceBetween: 15,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 15,
+        },
+        1024: {
+            slidesPerView: 4,
+            spaceBetween: 15,
+        },
+        1280: {
+            slidesPerView: 5,
+            spaceBetween: 15,
+        },
+    }
+    return(
+        <Fragment>
+            <div className="container py-4 px-4 justify-content-center">
+                <SpecialRowProduct breakpoints_data={config_screen_swiper} title_data="BÁN CHẠY"/>
+                <SpecialRowProduct breakpoints_data={config_screen_swiper} title_data="MỚI RA MẮT"/>
+
+                <div className="mini-adv">
+                    <div className="mini-adv-left">
+                        <img src="../../images/mini-banner/mini-2.png" alt=""/>
+                    </div>
+                    <div className="mini-adv-right">
+                        <img src="../../images/mini-banner/mini-3.png" alt=""/>
+                    </div>
+                </div>
+
+                <NormalRowProduct breakpoints_data={config_screen_swiper} title_data="VĂN HỌC"/>
+                <NormalRowProduct breakpoints_data={config_screen_swiper} title_data="NGOẠI NGỮ"/>
+                <NormalRowProduct breakpoints_data={config_screen_swiper} title_data="TIỂU THUYẾT"/>
+            </div>
+        </Fragment>
+    )
+}
+
+export default HomePage;
