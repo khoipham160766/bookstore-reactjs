ca65ba9e849dc6b45c352589bb7deb2ba8842bd2
Phạm Trần Khôi <82496203+khoipham160766@users.noreply.github.com>
2022-10-12 01:19:01 +0700

    update filter

---
+++ fe/src/components/user/filter/filterProduct.jsx
@@ -0,0 +1,83 @@
+import React, { Fragment } from "react";
+import "./style.css";
+
+const FilterProduct = () =>  {
+    return(
+        <Fragment>
+            <div className="border rounded shadow-sm filter-product">
+                <div className="title-filter-product">
+                    <h1>LỌC SẢN PHẨM</h1>
+                </div>
+                <ul className="list-group list-group-flush rounded list-filter">
+                    <li className="list-group-item">
+                        <h5 className="mt-1 mb-1">Loại sách</h5>
+                        <div className="d-flex flex-column">
+                            <div className="form-check">
+                                <input className="form-check-input" type="checkbox"></input>
+                                <label className="form-check-label">Sách văn học</label>
+                            </div>
+                            <div className="form-check">
+                                <input className="form-check-input" type="checkbox"></input>
+                                <label className="form-check-label">Sách văn học</label>
+                            </div>
+                            <div className="form-check">
+                                <input className="form-check-input" type="checkbox"></input>
+                                <label className="form-check-label">Sách văn học</label>
+                            </div>
+                        </div>
+                    </li>
+                    <li className="list-group-item">
+                        <h5 className="mt-1 mb-1">Nhà xuất bản</h5>
+                        <div className="d-flex flex-column">
+                            <div className="form-check">
+                                <input className="form-check-input" type="checkbox"></input>
+                                <label className="form-check-label">Sách văn học</label>
+                            </div>
+                            <div className="form-check">
+                                <input className="form-check-input" type="checkbox"></input>
+                                <label className="form-check-label">Sách văn học</label>
+                            </div>
+                            <div className="form-check">
+                                <input className="form-check-input" type="checkbox"></input>
+                                <label className="form-check-label">Sách văn học</label>
+                            </div>
+                        </div>
+                    </li>
+                    <li className="list-group-item">
+                        <h5 className="mt-1 mb-1">Tác giả</h5>
+                        <div className="d-flex flex-column">
+                            <div className="form-check">
+                                <input className="form-check-input" type="checkbox"></input>
+                                <label className="form-check-label">Sách văn học</label>
+                            </div>
+                            <div className="form-check">
+                                <input className="form-check-input" type="checkbox"></input>
+                                <label className="form-check-label">Sách văn học</label>
+                            </div>
+                            <div className="form-check">
+                                <input className="form-check-input" type="checkbox"></input>
+                                <label className="form-check-label">Sách văn học</label>
+                            </div>
+                        </div>
+                    </li>
+                    <li className="list-group-item">
+                        <h5 className="mt-1 mb-1">Khoảng giá</h5>
+                        <div className="d-grid d-block mb-3">
+                            <div className="form-floating mb-2">
+                                <input type="text" className="form-control" placeholder="Min" value="100000"/>
+                                <label for="floatingInput">Giá nhỏ nhất</label>
+                            </div>
+                            <div className="form-floating mb-2">
+                                <input type="text" className="form-control" placeholder="Max" value="500000"/>
+                                <label for="floatingInput">Giá lớn nhất</label>
+                            </div>
+                            <button className="btn btn-dark btn-dark-custom">LỌC</button>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </Fragment>
+    )
+}
+
+export default FilterProduct;
